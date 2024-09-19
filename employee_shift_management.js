f3ee65c16c2e64d949e3526d97e95fa5c71041ab
Breanna Burris <breanna1burris@gmail.com>
2024-09-19 15:56:42 -0400

    Initialize employees with shift schedules

---
+++ employee_shift_management.js
@@ -0,0 +1,33 @@
+// Task 1: Employees Array of Employee Objects
+
+let employees = [ // array for employee names and their shifts
+    {name: 'Alice'},
+    {shifts:[
+        {day: 'Monday', hours: 5 },
+        {day: 'Wednesday', hours: 10}
+    ]
+    },
+
+    {name: 'Bob'},
+    {shifts:[
+        {day: 'Tuesday', hours: 2 },
+        {day: 'Thursday', hours: 13}
+    ]
+    },
+
+    {name: 'Charlie'},
+    {shifts:[
+        {day: 'Monday', hours: 7 },
+        {day: 'Wednesday', hours: 8}
+    ]
+    },
+
+    {name: 'Eve'},
+    {shifts: [
+        {day: 'Tuesday', hours: 5 },
+        {day: 'Friday', hours: 10}
+    ]
+}];
+
+console.log(employees);
+
